7102412a710b51d7e8a09c515a9088a5b8b1a19d
Neelima_Personal <neelima_personal@Neelimas-Air.fritz.box>
2022-12-06 19:11:59 +0000

    Function to load array of packages

--- requirements/load_packages.R
+++ requirements/load_packages.R
@@ -1,1 +1,13 @@
+load_packages <- function(x){
+  for( i in x ){
+    #  require returns TRUE invisibly if it was able to load package
+    if( ! require( i , character.only = TRUE ) ){
+      #  If package was not able to be loaded then re-install
+      install.packages( i , dependencies = TRUE )
+      #  Load package after installing
+      require( i , character.only = TRUE )
+    }
+  }
+}
+
 
